d272257e7a6f9f4d811c457f992db7a49c169fcb
Yulia Lebedeva <yulia.lebedeva@jugru.org>
2024-03-17 14:09:15 +0300

    added setup data and setup file

--- tests/homework_lesson19.spec.ts
+++ tests/homework_lesson19.spec.ts
@@ -1,27 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Unauthorised user', () => {
+test.describe.skip('Unauthorised user', () => {
     test('should see BUY A TICKET in hero block', async ({ page }) => {
       await page.goto('');
 
       await expect(page.locator('#main__anchor').getByRole('link', { name: 'Купить билет' })).toBeVisible(); 
     });
     
-      test('should NOT see BUY A TICKET in hero menu -> more', async ({ page }) => {
+      test('should see BUY A TICKET in hero menu', async ({ page }) => {
         await page.goto('');
-        await page.getByLabel('Еще').click();
-        await expect(page.getByLabel('Основная').getByRole('link', { name: 'Купить билет' })).toBeVisible();
+        await expect(page.getByLabel('Важные разделы').getByRole('link', { name: 'Купить билет' })).toBeVisible();
       });
+
+      // page.getByLabel('Важные разделы').getByRole('link', { name: 'Купить билет' })
   });
 
-//   test.describe('Authorised user', () => {
-//     test('should see SCHEDULE in hero block', async ({ page }) => {
-//       // ...
-//     });
+  test.describe('Authorised user with ticket', () => {
+    test('should NOT see BUY A TICKET in hero block', async ({ browser }) => {
+      const withTIcketContext = await browser.newContext({ storageState: 'playwright/.auth/hasTicket.json' });
+      const withTicketPage = await withTIcketContext.newPage();
+      await withTicketPage.goto('');
+
+      await expect(withTicketPage.locator('#main__anchor').getByRole('link', { name: 'Купить билет' })).not.toBeVisible(); 
+      await withTIcketContext.close();
   
-//     test('should NOT see BUY A TICKET in hero block', async ({ page }) => {
-//       // ...
-//     });
+    });
+    // });
 
 //     test('should NOT see BUY A TICKET in hero menu', async ({ page }) => {
 //         // ...
@@ -30,4 +34,16 @@ test.describe('Unauthorised user', () => {
 //     test('should see BUY A TICKET in hero menu -> more', async ({ page }) => {
 //         // ...
 //       });
-//   });
+  });
+
+  test.describe('Authorised user without ticket', () => {
+    test('should see BUY A TICKET in hero block', async ({ browser }) => {
+      const withTIcketContext = await browser.newContext({ storageState: 'playwright/.auth/noTicket.json' });
+      const withTicketPage = await withTIcketContext.newPage();
+      await withTicketPage.goto('');
+
+      await expect(withTicketPage.locator('#main__anchor').getByRole('link', { name: 'Купить билет' })).toBeVisible(); 
+      await withTIcketContext.close();
+  
+    });
+  });
