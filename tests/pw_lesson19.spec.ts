fa87cfc6912d1c78fe73391f4e57b2932dc68964
Yulia Lebedeva <yulia.lebedeva@jugru.org>
2024-03-19 01:21:18 +0300

    test for unauthorised user

--- tests/pw_lesson19.spec.ts
+++ tests/pw_lesson19.spec.ts
@@ -1,24 +1,25 @@
 import { test, expect } from '@playwright/test';
 import { users } from '../fixtures/.authData';
+import { beforeEach } from 'node:test';
 
 test.describe('Check BUY A TICKET button on main page', () => {
-    test('User with a ticket', async({ page }) => {
-        test.setTimeout(50000);
+    test.beforeEach(async ({ page } ) => {
         await page.goto('');
         await expect(page.getByRole('link', { name: 'Войти' })).toBeVisible();
-
-    // Check page for unauthorised user
-        // Should see BUY A TICKET in hero block
-        await expect(page.locator('#main__anchor').getByRole('link', { name: 'Купить билет' })).toBeVisible();
-        // Should see BUY A TICKET in menu line
-        await expect(page.getByLabel('Важные разделы').getByRole('link', { name: 'Купить билет' })).toBeVisible();
-        // Should NOT see BUY A TICKET in menu -> more
-        await page.getByLabel('Еще').click();
-        const count = await page.locator('#headerNavigation__more').getByRole('listitem').count();
-        for (let i = 0; i < count; ++i)
-            await expect(page.locator('#headerNavigation__more').getByRole('listitem').nth(i)).not.toHaveText('Купить билет');
-
-    // Authorise and check page
+      });
+    test('Unauthorised user', async({ page }) => {
+      // Should see BUY A TICKET in hero block
+      await expect(page.locator('#main__anchor').getByRole('link', { name: 'Купить билет' })).toBeVisible();
+      // Should see BUY A TICKET in menu line
+      await expect(page.getByLabel('Важные разделы').getByRole('link', { name: 'Купить билет' })).toBeVisible();
+      // Should NOT see BUY A TICKET in menu -> more
+      await page.getByLabel('Еще').click();
+      const count = await page.locator('#headerNavigation__more').getByRole('listitem').count();
+      for (let i = 0; i < count; ++i)
+          await expect(page.locator('#headerNavigation__more').getByRole('listitem').nth(i)).not.toHaveText('Купить билет');
+    })
+    test('User with a ticket', async({ page }) => {
+    // Authorise
         await page.getByRole('link', { name: 'Войти' }).click();
 
         await page.locator('#login-email').fill(`${users[0].login}`);
@@ -35,9 +36,6 @@ test.describe('Check BUY A TICKET button on main page', () => {
     });
 
     test('User without a ticket', async({ page }) => {
-        await page.goto('');
-        await expect(page.getByRole('link', { name: 'Войти' })).toBeVisible();
-
         await page.getByRole('link', { name: 'Войти' }).click();
 
         await page.locator('#login-email').fill(`${users[1].login}`);
